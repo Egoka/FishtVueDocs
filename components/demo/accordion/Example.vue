6d1ae5b02e1780c1ab5765b3f4b993f400c15a7a
Egoka <eg.bonda2014@gmail.com>
2025-05-14 21:44:52 +0300

    fix(locale)

---
+++ components/demo/accordion/Example.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {useI18n} from "vue-i18n";
+
+const {t} = useI18n()
+const dataSource = ref([
+  {
+    title: t("start.FAQ.title.Q1"),
+    subtitle: t("start.FAQ.subtitle.Q1"),
+    open: false
+  },
+  {
+    title: t("start.FAQ.title.Q2"),
+    subtitle: t("start.FAQ.subtitle.Q2"),
+    open: false
+  },
+]);
+</script>
+<template>
+  <Accordion :dataSource="dataSource"/>
+</template>
